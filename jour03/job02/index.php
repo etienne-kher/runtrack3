4c25fd961ffee3d33fee3fe9b62e265aa46a5c73
Kher <etienne.kher@laplateforme.io>
2020-03-11 21:21:58 +0100

    job2

---
+++ jour03/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job02</title>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script  type="text/javascript" src="script.js" async="true"></script>
+</head>
+<body>
+	<div id="melangees"></div>
+	<button id="button">melangeur</button>
+	<div id="rangees"></div>
+	<h1></h1>
+</body>
+</html>

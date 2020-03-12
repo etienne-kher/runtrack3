f37fbf340c51c4dedc61a0ab56252ee27298e443
Kher <etienne.kher@laplateforme.io>
2020-03-12 18:38:53 +0100

    j4

---
+++ jour04/job01/script.js
@@ -0,0 +1,12 @@
+$('#button').click(function lecture()
+{
+		$.ajax(
+		{
+			url:'expression.txt',
+			datatype:'text',
+			success: function(data)
+			{	$('body').append('<p></p>');
+				$('p').text(data);	
+			}
+		});
+});
